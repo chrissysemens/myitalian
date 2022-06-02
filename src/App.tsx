a723587be286f3bb7462f7a3c3f5178082b8be86
Chrissy Semens <chrissy.semens@assetfiftyfive.com>
2022-06-03 00:12:16 +0100

    Tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./App.module.scss";
+import { Card, CardProps } from "./Components/Card";
+import { Image } from "./Components/Image";
 import { useFirebase } from "./Hooks/useFirebase";
+import back from "./assets/back.svg";
+import forward from "./assets/forward.svg";
+import shuffle from "lodash/shuffle";
 
 function App() {
-  const { data } = useFirebase({ collectionName: "Translations" });
+  const { data } = useFirebase<CardProps>({ collectionName: "Translations" });
+  const [number, setNumber] = useState<number>(0);
+  const [cards, setCards] = useState<Array<CardProps>>([]);
 
-  console.log(data);
+  useEffect(() => {
+    const shuffled = shuffle(data);
+    console.log('shuffled', shuffled);
+    setCards(shuffled);
+  }, [data]);
 
-  return <div className={styles.app}>x</div>;
+  const next = () => {
+    const i = number + 1 > data.length - 1 ? 0 : number + 1;
+    setNumber(i);
+  };
+
+  const previous = () => {
+    const i = number - 1 < 0 ? data.length - 1 : number - 1;
+    setNumber(i);
+  };
+
+  return (
+    <div className={styles.app}>
+      {cards[number] && (
+        <>
+          <div className={styles.card}>
+            <Card
+              italian={cards[number].italian}
+              english={cards[number].english}
+            />
+          </div>
+          <div className={styles.buttons}>
+            <div className={styles.back} onClick={() => previous()}>
+              <Image src={back} alt="Previous" height={80} width={80} />
+            </div>
+            <div className={styles.forward} onClick={() => next()}>
+              <Image src={forward} alt="Next" height={80} width={80} />
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Card.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./Card.module.scss";
+
+export type CardProps = {
+  english: string;
+  italian: string;
+};
+
+const Card = ({ english, italian }: CardProps) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.translation}>
+        <div className={styles.english}>
+          <div className={styles.heading}>English</div>
+          <div className={styles.word}>{english}</div>
+        </div>
+        <div className={styles.italian}>
+          <div className={styles.heading}>Italian</div>
+          <div className={styles.word}>{italian}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { Card };
