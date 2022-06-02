32d2a5943c23def0ea75879dbe2e8c53e8593e23
Chrissy Semens <chrissy.semens@assetfiftyfive.com>
2022-06-03 00:31:48 +0100

    .env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqv0LII_F76QO4ojqwGfGuxU2eau-O2ng",
-  authDomain: "myitalian-8761c.firebaseapp.com",
-  projectId: "myitalian-8761c",
-  storageBucket: "myitalian-8761c.appspot.com",
-  messagingSenderId: "67022693297",
-  appId: "1:67022693297:web:768d5ead0a6423555a9390",
-  measurementId: "G-595ZYW12G4",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const firebase = initializeApp(firebaseConfig);
